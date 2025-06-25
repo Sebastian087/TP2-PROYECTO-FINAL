23e3707fa2dd58eb78f08fdfa1e5c189bc5b5efb
Sebastian087 <sebastianelias087@gmail.com>
2025-06-25 19:22:57 -0300

    Ultimos cambios

--- src/config/config.js
+++ src/config/config.js
@@ -4,7 +4,6 @@ dotenv.config();
 export const config = {
   PORT: process.env.PORT || 3000,
   HOST: process.env.HOST || "localhost",
-  DB_PATH: process.env.DB_PATH || "./src/db/pelicula.db.json",
   SUPABASE_URL: process.env.SUPABASE_URL,
   SUPABASE_KEY: process.env.SUPABASE_KEY,
   BASIC_AUTH_USER: process.env.BASIC_AUTH_USER,
